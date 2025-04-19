e0d5e2b36e5fa9517221b405e75cd9721f86ee5e
louislefo <louis.le_forestier@edu.devinci.fr>
2025-04-19 11:37:13 +0200

    Ajout classe requettes. a modifier

---
+++ LivrableV3/LivrableV3/SqlClient.cs
@@ -0,0 +1,155 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrableV3
+{
+    public class SqlClient
+    {
+        public ConnexionBDDClient connexionBDDClient;
+
+        public SqlClient(ConnexionBDDClient connexionBDDClient)
+        {
+            this.connexionBDDClient = connexionBDDClient;
+        }
+
+
+        public void VoirPlatsDisponibles()
+        {
+            try
+            {
+
+                string requete = "SELECT Plat_.id_plat, Plat_.nom as nom_plat, Plat_.type, Plat_.prix_par_personne, utilisateur.nom as nom_cuisinier, utilisateur.prénom " +
+                               "FROM Plat_, cuisinier, utilisateur " +
+                               "WHERE Plat_.id_cuisinier = cuisinier.id_cuisinier " +
+                               "AND cuisinier.id_utilisateur = utilisateur.id_utilisateur";
+
+                MySqlCommand commande = new MySqlCommand(requete, connexionBDDClient.maConnexionClient);
+                commande.CommandText = requete;
+
+                MySqlDataReader reader = commande.ExecuteReader();
+
+                Console.WriteLine("\nvoici les plats disponibles");
+                Console.WriteLine("----------------------------------");
+
+                while (reader.Read())
+                {
+                    string idPlat = reader["id_plat"].ToString();
+                    string nomPlat = reader["nom_plat"].ToString();
+                    string type = reader["type"].ToString();
+                    string prix = reader["prix_par_personne"].ToString();
+                    string nomCuisinier = reader["nom_cuisinier"].ToString();
+                    string prenomCuisinier = reader["prénom"].ToString();
+
+                    Console.WriteLine("Plat numero " + idPlat);
+                    Console.WriteLine("Nom: " + nomPlat);
+                    Console.WriteLine("Type: " + type);
+                    Console.WriteLine("Prix: " + prix + " euros");
+                    Console.WriteLine("Cuisinier: " + prenomCuisinier + " " + nomCuisinier);
+                    Console.WriteLine("----------------------------------");
+                }
+
+                reader.Close();
+                commande.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("oups ya une erreur : " + ex.Message);
+            }
+        }
+
+
+        public void VoirCommandesClient(string idClient)
+        {
+            try
+            {
+
+                string requete = "SELECT Commande_.id_commande, date_commande, prix_total, statut, Plat_.nom as nom_plat " +
+                               "FROM Commande_, Plat_ " +
+                               "WHERE Commande_.id_plat = Plat_.id_plat " +
+                               "AND Commande_.id_client = '" + idClient + "' " +
+                               "ORDER BY date_commande DESC";
+
+                MySqlCommand commande = new MySqlCommand(requete, connexionBDDClient.maConnexionClient);
+                commande.CommandText = requete;
+
+                MySqlDataReader reader = commande.ExecuteReader();
+
+                Console.WriteLine("\nvoici vos commandes");
+                Console.WriteLine("----------------------------------");
+
+                while (reader.Read())
+                {
+                    string idCommande = reader["id_commande"].ToString();
+                    string date = reader["date_commande"].ToString();
+                    string prix = reader["prix_total"].ToString();
+                    string statut = reader["statut"].ToString();
+                    string nomPlat = reader["nom_plat"].ToString();
+
+                    Console.WriteLine("Commande numero " + idCommande);
+                    Console.WriteLine("Date: " + date);
+                    Console.WriteLine("Plat: " + nomPlat);
+                    Console.WriteLine("Prix: " + prix + " euros");
+                    Console.WriteLine("Statut: " + statut);
+                    Console.WriteLine("----------------------------------");
+                }
+
+                reader.Close();
+                commande.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("oups ya une erreur : " + ex.Message);
+            }
+        }
+
+
+        public void PasserCommande(string idClient)
+        {
+            try
+            {
+                Console.WriteLine("veuillez entrer l'id du plat que vous voulez commander");
+                string idPlat = Console.ReadLine();
+
+                string requetePlat = "SELECT id_cuisinier, prix_par_personne FROM Plat_ WHERE id_plat = '" + idPlat + "'";
+                MySqlCommand commandePlat = new MySqlCommand(requetePlat, connexionBDDClient.maConnexionClient);
+                MySqlDataReader readerPlat = commandePlat.ExecuteReader();
+
+                if (readerPlat.Read())
+                {
+                    string idCuisinier = readerPlat["id_cuisinier"].ToString();
+                    double prix = Convert.ToDouble(readerPlat["prix_par_personne"]);
+
+                    readerPlat.Close();
+                    commandePlat.Dispose();
+
+                    string idCommande = "CMD" + DateTime.Now.ToString("yyyyMMddHHmmss");
+
+                    string requeteCommande = "INSERT INTO Commande_ VALUES ('" + idCommande + "', '" + idClient + "', '" + idCuisinier + "', '" + idPlat + "', '" +
+                                          DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "', " + prix.ToString().Replace(',', '.') + ", 'En attente')";
+
+                    MySqlCommand commandeInsert = new MySqlCommand(requeteCommande, connexionBDDClient.maConnexionClient);
+                    commandeInsert.ExecuteNonQuery();
+                    commandeInsert.Dispose();
+
+                    Console.WriteLine("votre commande a ete passee avec succes");
+                    Console.WriteLine("numero de commande : " + idCommande);
+                }
+                else
+                {
+                    readerPlat.Close();
+                    commandePlat.Dispose();
+                    Console.WriteLine("ce plat nexiste pas");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("oups ya une erreur : " + ex.Message);
+            }
+        }
+
+    }
+}
---
+++ LivrableV3/LivrableV3/SqlCuisinier.cs
@@ -0,0 +1,172 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrableV3
+{
+    public class SqlCuisinier
+    {
+        public ConnexionBDDCuisinier connexionBDD;
+
+        public SqlCuisinier(ConnexionBDDCuisinier connexionBDD)
+        {
+            this.connexionBDD = connexionBDD;
+        }
+
+        public void AjouterPlat(string idCuisinier)
+        {
+            try
+            {
+                Console.WriteLine("\nAjout d'un nouveau plat");
+                Console.WriteLine("----------------------");
+
+                Console.Write("nom du plat : ");
+                string nomPlat = Console.ReadLine();
+
+                Console.Write("type de plat (entree, plat, dessert) : ");
+                string typePlat = Console.ReadLine();
+
+                Console.Write("nombre de portions : ");
+                string portions = Console.ReadLine();
+
+                Console.Write("date de fabrication (jj/mm/aaaa) : ");
+                DateTime dateFabrication = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+                Console.Write("date de peremption (jj/mm/aaaa) : ");
+                DateTime datePeremption = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+                Console.Write("prix par personne (en euros) : ");
+                double prixParPersonne = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("nationalite du plat : ");
+                string nationalite = Console.ReadLine();
+
+                Console.Write("regime (normal, vegetarien, vegan) : ");
+                string regime = Console.ReadLine();
+
+                Console.Write("photo (nom du fichier) : ");
+                string photo = Console.ReadLine();
+
+                string idPlat = "PLT" + DateTime.Now.ToString("yyyyMMddHHmmss");
+
+                string requete = "INSERT INTO Plat_ VALUES ('" + idPlat + "', '" + idCuisinier + "', '" +
+                               nomPlat + "', '" + typePlat + "', '" + portions + "', '" +
+                               dateFabrication.ToString("yyyy-MM-dd") + "', '" +
+                               datePeremption.ToString("yyyy-MM-dd") + "', " +
+                               prixParPersonne.ToString().Replace(',', '.') + ", '" +
+                               nationalite + "', '" + regime + "', '" + photo + "')";
+
+                MySqlCommand commande = new MySqlCommand(requete, connexionBDD.maConnexionCuisinier);
+                commande.CommandText = requete;
+                commande.ExecuteNonQuery();
+                commande.Dispose();
+
+                Console.WriteLine("plat ajoute avec succes");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("oups ya une erreur : " + ex.Message);
+            }
+        }
+
+        public void VoirMesPlats(string idCuisinier)
+        {
+            try
+            {
+                string requete = "SELECT id_plat, nom, type, portions, date_fabrication, date_peremption, " +
+                               "prix_par_personne, nationalite, regime FROM Plat_ " +
+                               "WHERE id_cuisinier = '" + idCuisinier + "'";
+
+                MySqlCommand commande = new MySqlCommand(requete, connexionBDD.maConnexionCuisinier);
+                commande.CommandText = requete;
+
+                MySqlDataReader reader = commande.ExecuteReader();
+
+                Console.WriteLine("\nvoici vos plats");
+                Console.WriteLine("----------------------------------");
+
+                while (reader.Read())
+                {
+                    string idPlat = reader["id_plat"].ToString();
+                    string nom = reader["nom"].ToString();
+                    string type = reader["type"].ToString();
+                    string portions = reader["portions"].ToString();
+                    string dateFabrication = Convert.ToDateTime(reader["date_fabrication"]).ToShortDateString();
+                    string datePeremption = Convert.ToDateTime(reader["date_peremption"]).ToShortDateString();
+                    double prix = Convert.ToDouble(reader["prix_par_personne"]);
+                    string nationalite = reader["nationalite"].ToString();
+                    string regime = reader["regime"].ToString();
+
+                    Console.WriteLine("Plat " + idPlat + " : " + nom);
+                    Console.WriteLine("Type : " + type);
+                    Console.WriteLine("Portions : " + portions);
+                    Console.WriteLine("Fabrication : " + dateFabrication);
+                    Console.WriteLine("Peremption : " + datePeremption);
+                    Console.WriteLine("Prix : " + prix + " euros");
+                    Console.WriteLine("Nationalite : " + nationalite);
+                    Console.WriteLine("Regime : " + regime);
+                    Console.WriteLine("----------------------------------");
+                }
+
+                reader.Close();
+                commande.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("oups ya une erreur : " + ex.Message);
+            }
+        }
+
+        public void VoirCommandesEnCours(string idCuisinier)
+        {
+            try
+            {
+                string requete = "SELECT c.id_commande, c.date_commande, c.prix_total, c.statut, " +
+                               "p.nom as nom_plat, u.nom as nom_client, u.prénom as prenom_client " +
+                               "FROM Commande_ c " +
+                               "JOIN Plat_ p ON c.id_plat = p.id_plat " +
+                               "JOIN client cl ON c.id_client = cl.id_client " +
+                               "JOIN utilisateur u ON cl.id_utilisateur = u.id_utilisateur " +
+                               "WHERE c.id_cuisinier = '" + idCuisinier + "' " +
+                               "AND c.statut != 'Terminée'";
+
+                MySqlCommand commande = new MySqlCommand(requete, connexionBDD.maConnexionCuisinier);
+                commande.CommandText = requete;
+
+                MySqlDataReader reader = commande.ExecuteReader();
+
+                Console.WriteLine("\nvoici vos commandes en cours");
+                Console.WriteLine("----------------------------------");
+
+                while (reader.Read())
+                {
+                    string idCommande = reader["id_commande"].ToString();
+                    string dateCommande = Convert.ToDateTime(reader["date_commande"]).ToString();
+                    double prix = Convert.ToDouble(reader["prix_total"]);
+                    string statut = reader["statut"].ToString();
+                    string nomPlat = reader["nom_plat"].ToString();
+                    string nomClient = reader["nom_client"].ToString();
+                    string prenomClient = reader["prenom_client"].ToString();
+
+                    Console.WriteLine("Commande " + idCommande);
+                    Console.WriteLine("Date : " + dateCommande);
+                    Console.WriteLine("Plat : " + nomPlat);
+                    Console.WriteLine("Client : " + prenomClient + " " + nomClient);
+                    Console.WriteLine("Prix : " + prix + " euros");
+                    Console.WriteLine("Statut : " + statut);
+                    Console.WriteLine("----------------------------------");
+                }
+
+                reader.Close();
+                commande.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("oups ya une erreur : " + ex.Message);
+            }
+        }
+    }
+}
