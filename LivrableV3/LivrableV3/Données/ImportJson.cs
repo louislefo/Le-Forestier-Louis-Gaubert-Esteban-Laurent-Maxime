b4e8b6a798beab805a3fe0db83675ea73912aa4a
maximelrnt <maxime.laurent@edu.devinci.fr>
2025-05-02 00:09:42 +0200

    peut etre erreur de package verifie que tu as xml et esteban json aussi
    simplification pour utiliser une classe et eviter les ambiguités entre xml et json

--- LivrableV3/LivrableV3/Données/ImportJson.cs
+++ LivrableV3/LivrableV3/Données/ImportJson.cs
@@ -6,13 +6,13 @@ using MySql.Data.MySqlClient;
 
 namespace LivrableV3
 {
-
+    /// classe pour importer les donnees json
     public class ImportJson
     {
         public string cheminFichierJson;
         public ConnexionBDD connexion;
 
-        /// c la classe pour importer json
+        /// constructeur de la classe
         public ImportJson(ConnexionBDD connexionBase)
         {
             this.cheminFichierJson = @"../../Données/Donnee.json";
@@ -20,61 +20,76 @@ namespace LivrableV3
             ImporterTout();
         }
 
-        /// pour importer tout
+        /// methode pour importer toutes les donnees
         public void ImporterTout()
         {
             string texteJson = File.ReadAllText(cheminFichierJson);
             DonneesImportees donnees = JsonSerializer.Deserialize<DonneesImportees>(texteJson);
 
-            // utilisateur
+            // import des utilisateurs
             for (int i = 0; i < donnees.utilisateurs.Count; i++)
             {
                 Utilisateur utilisateur = donnees.utilisateurs[i];
-                string requete = "INSERT INTO utilisateur (id_utilisateur, nom, prénom, email, adresse, telephone, mot_de_passe) VALUES ('" + utilisateur.id_utilisateur + "', '" + utilisateur.nom + "', '" + utilisateur.prenom + "', '" + utilisateur.email + "', '" + utilisateur.adresse + "', '" + utilisateur.telephone + "', '" + utilisateur.mot_de_passe + "');";
+                string requete = "INSERT INTO utilisateur (id_utilisateur, nom, prénom, email, adresse, telephone, mot_de_passe) VALUES ('" + 
+                    utilisateur.id_utilisateur + "', '" + utilisateur.nom + "', '" + utilisateur.prenom + "', '" + 
+                    utilisateur.email + "', '" + utilisateur.adresse + "', '" + utilisateur.telephone + "', '" + 
+                    utilisateur.mot_de_passe + "');";
                 MySqlCommand commande = new MySqlCommand(requete, connexion.maConnexion);
                 commande.CommandText = requete;
                 commande.ExecuteNonQuery();
                 commande.Dispose();
             }
 
-            // client
+            // import des clients
             for (int i = 0; i < donnees.clients.Count; i++)
             {
                 Client client = donnees.clients[i];
-                string requete = "INSERT INTO client (id_client, id_utilisateur, StationMetro, entreprise_nom, referent) VALUES ('" + client.id_client + "', '" + client.id_utilisateur + "', '" + client.StationMetro + "', '" + client.entreprise_nom + "', '" + client.referent + "');";
+                string entrepriseNom = string.IsNullOrEmpty(client.entreprise_nom) ? "NULL" : "'" + client.entreprise_nom + "'";
+                string referent = string.IsNullOrEmpty(client.referent) ? "NULL" : "'" + client.referent + "'";
+                
+                string requete = "INSERT INTO client (id_client, id_utilisateur, StationMetro, entreprise_nom, referent) VALUES ('" + 
+                    client.id_client + "', '" + client.id_utilisateur + "', '" + client.StationMetro + "', " + 
+                    entrepriseNom + ", " + referent + ");";
                 MySqlCommand commande = new MySqlCommand(requete, connexion.maConnexion);
                 commande.CommandText = requete;
                 commande.ExecuteNonQuery();
                 commande.Dispose();
             }
 
-            // cuisinier
+            // import des cuisiniers
             for (int i = 0; i < donnees.cuisiniers.Count; i++)
             {
                 Cuisinier cuisinier = donnees.cuisiniers[i];
-                string requete = "INSERT INTO cuisinier (id_cuisinier, id_utilisateur, StationMetro, zones_livraison, note_moyenne, nombre_livraisons) VALUES ('" + cuisinier.id_cuisinier + "', '" + cuisinier.id_utilisateur + "', '" + cuisinier.StationMetro + "', '" + cuisinier.zones_livraison + "', " + cuisinier.note_moyenne + ", " + cuisinier.nombre_livraisons + ");";
+                string requete = "INSERT INTO cuisinier (id_cuisinier, id_utilisateur, StationMetro, zones_livraison, note_moyenne, nombre_livraisons) VALUES ('" + 
+                    cuisinier.id_cuisinier + "', '" + cuisinier.id_utilisateur + "', '" + cuisinier.StationMetro + "', '" + 
+                    cuisinier.zones_livraison + "', " + cuisinier.note_moyenne + ", " + cuisinier.nombre_livraisons + ");";
                 MySqlCommand commande = new MySqlCommand(requete, connexion.maConnexion);
                 commande.CommandText = requete;
                 commande.ExecuteNonQuery();
                 commande.Dispose();
             }
 
-            // commande
+            // import des commandes
             for (int i = 0; i < donnees.commandes.Count; i++)
             {
-                Commande commandeObj = donnees.commandes[i];
-                string requete = "INSERT INTO Commande_ (id_commande, id_client, id_cuisinier, id_plat, date_commande, prix_total, statut) VALUES ('" + commandeObj.id_commande + "', '" + commandeObj.id_client + "', '" + commandeObj.id_cuisinier + "', '" + commandeObj.id_plat + "', '" + commandeObj.date_commande + "', " + commandeObj.prix_total + ", '" + commandeObj.statut + "');";
-                MySqlCommand commande = new MySqlCommand(requete, connexion.maConnexion);
-                commande.CommandText = requete;
-                commande.ExecuteNonQuery();
-                commande.Dispose();
+                Commande commande = donnees.commandes[i];
+                string requete = "INSERT INTO Commande_ (id_commande, id_client, id_cuisinier, id_plat, date_commande, prix_total, statut) VALUES ('" + 
+                    commande.id_commande + "', '" + commande.id_client + "', '" + commande.id_cuisinier + "', '" + 
+                    commande.id_plat + "', '" + commande.date_commande + "', " + commande.prix_total + ", '" + 
+                    commande.statut + "');";
+                MySqlCommand cmd = new MySqlCommand(requete, connexion.maConnexion);
+                cmd.CommandText = requete;
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
             }
 
-            // livraison
+            // import des livraisons
             for (int i = 0; i < donnees.livraisons.Count; i++)
             {
                 Livraison livraison = donnees.livraisons[i];
-                string requete = "INSERT INTO Livraison (id_livraison, id_commande, date_livraison, trajet) VALUES ('" + livraison.id_livraison + "', '" + livraison.id_commande + "', '" + livraison.date_livraison + "', '" + livraison.trajet + "');";
+                string requete = "INSERT INTO Livraison (id_livraison, id_commande, date_livraison, trajet) VALUES ('" + 
+                    livraison.id_livraison + "', '" + livraison.id_commande + "', '" + livraison.date_livraison + "', '" + 
+                    livraison.trajet + "');";
                 MySqlCommand commande = new MySqlCommand(requete, connexion.maConnexion);
                 commande.CommandText = requete;
                 commande.ExecuteNonQuery();
@@ -140,5 +155,4 @@ namespace LivrableV3
         public string date_livraison;
         public string trajet;
     }
-
 }
